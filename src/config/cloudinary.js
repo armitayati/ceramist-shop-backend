2775f0402c85f749749510c728cff96aa2f20947
armitayati <95957128+armitayati@users.noreply.github.com>
2025-10-24 17:41:07 +0200

    111

---
+++ src/config/cloudinary.js
@@ -0,0 +1,6 @@
+// Configuración de Cloudinary
+export const CLOUDINARY_CONFIG = {
+  CLOUD_NAME: 'dhdvavzrh', 
+  UPLOAD_PRESET: 'MitaPreset',  // Lo obtendrás de Cloudinary
+  API_KEY: '215259595879526'        
+};
